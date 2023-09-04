d2af63d596e2efe96748ee0454889e762834ef2e
Quincy Chisaina <113616008+Chisaina69@users.noreply.github.com>
2023-09-04 10:58:02 +0300

    Complet

---
+++ app/migrations/versions/9ca926c7984f_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 9ca926c7984f
+Revises: 
+Create Date: 2023-09-04 10:48:52.342261
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9ca926c7984f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
